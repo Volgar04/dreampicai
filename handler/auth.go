83b630e385fcba3bd31d42151aaaf6e3f366ab77
Nicolas <nicolasmartin0404@gmail.com>
2024-03-07 14:34:14 +0100

    signup page done + validate package created

--- handler/auth.go
+++ handler/auth.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Volgar04/dreampicai/pkg/sb"
-	"github.com/Volgar04/dreampicai/pkg/util"
+	"github.com/Volgar04/dreampicai/pkg/validate"
 	"github.com/Volgar04/dreampicai/view/auth"
 	"github.com/nedpals/supabase-go"
 )
@@ -14,20 +14,41 @@ func HandleLoginIndex(w http.ResponseWriter, r *http.Request) error {
 	return render(r, w, auth.Login())
 }
 
+func HandleSignupIndex(w http.ResponseWriter, r *http.Request) error {
+	return render(r, w, auth.Signup())
+}
+
+func HandleSignupCreate(w http.ResponseWriter, r *http.Request) error {
+	params := auth.SignupParams{
+		Email:           r.FormValue("email"),
+		Password:        r.FormValue("password"),
+		ConfirmPassword: r.FormValue("confirmPassword"),
+	}
+	errors := auth.SignupErrors{}
+	if ok := validate.New(&params, validate.Fields{
+		"Email":    validate.Rules(validate.Email),
+		"Password": validate.Rules(validate.Password),
+		"ConfirmPassword": validate.Rules(
+			validate.Equal(params.Password),
+			validate.Message("Passwords do not match"),
+		),
+	}).Validate(&errors); !ok {
+		return render(r, w, auth.SignupForm(params, errors))
+	}
+	user, err := sb.Client.Auth.SignUp(r.Context(), supabase.UserCredentials{
+		Email:    params.Email,
+		Password: params.Password,
+	})
+	if err != nil {
+		return err
+	}
+	return render(r, w, auth.SignupSuccess(user.Email))
+}
+
 func HandleLoginCreate(w http.ResponseWriter, r *http.Request) error {
 	credentials := supabase.UserCredentials{
 		Email:    r.FormValue("email"),
 		Password: r.FormValue("password"),
-	}
-	if !util.IsValidateEmail(credentials.Email) {
-		return render(r, w, auth.LoginForm(credentials, auth.LoginErrors{
-			Email: "Please enter a valid email",
-		}))
-	}
-	if reason, ok := util.ValidatePassword(credentials.Password); !ok {
-		return render(r, w, auth.LoginForm(credentials, auth.LoginErrors{
-			Password: reason,
-		}))
 	}
 	resp, err := sb.Client.Auth.SignIn(r.Context(), credentials)
 	if err != nil {
@@ -44,7 +65,6 @@ func HandleLoginCreate(w http.ResponseWriter, r *http.Request) error {
 		Secure:   true,
 	}
 	http.SetCookie(w, cookie)
-
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 	return nil
 }
